1d11d160ba9ec059116b77ea14ab46e46ed251b6
JaguatiricaS <105946439+JaguatiricaS@users.noreply.github.com>
2022-08-25 14:25:20 -0300

    Add files via upload
    O Mini game de pedra papel e tesoura que eu desenvolvi

---
+++ Script.js
@@ -0,0 +1,65 @@
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]');
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
+
+  {
+    name: 'paper',
+    emoji: '✋',
+    vence: 'rock'
+  },
+  
+  {
+    name: 'scissor',
+    emoji: '✌',
+    vence: 'paper'
+  },
+  
+  {
+    name: 'rock',
+    emoji: '✊',
+    vence: 'scissor'
+  }
+
+]
+
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', e => {
+    const selectionName = selectionButton.dataset.selection
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
+
+function addSelectionResult(selection, winner){
+  const div = document.createElement('div')
+  div.innerText = selection.emoji
+  div.classList.add('result-selection')
+  if(winner) div.classList.add('winner')
+  finalColumn.after(div)
+}
+
+function incrementScore(scoreSpan){
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function makeSelection(selection){
+  const computerSelection = randomSelection()
+  const yourWinner = isWinner(selection, computerSelection)
+  const computerWinner = isWinner(computerSelection, selection)
+  
+  addSelectionResult(computerSelection, computerWinner)
+  addSelectionResult(selection, yourWinner)
+
+  if(yourWinner) incrementScore(yourScoreSpan)
+  if(computerWinner) incrementScore(computerScoreSpan)
+} 
+
+function isWinner(selection, opponentSelection){
+  return selection.vence === opponentSelection.name
+}
+function randomSelection(){
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+  return SELECTIONS[randomIndex]
+}
